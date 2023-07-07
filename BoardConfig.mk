20858f6a4df6cb2a976b1c0fce7f419a79b818d9
Arian <arian.kulmer@web.de>
2023-07-07 17:34:56 +0200

    davinci: Get rid of libinit
    Change-Id: I34dcb53ccd17ab9c1b460ab72fca7d2ba56b19e7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,10 +29,6 @@ PRODUCT_FS_COMPRESSION := 1
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_davinci
-TARGET_RECOVERY_DEVICE_MODULES := libinit_davinci
-
 # Kernel
 TARGET_KERNEL_CONFIG := davinci_defconfig
 
